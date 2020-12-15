c38866989d2e378086285efa2d476cbce7db5018
zahrakhadijha <zahra.khan1@icloud.com>
2020-12-14 23:59:20 -0500

    nav styling with styled components

--- book-app/src/Components/Header.jsx
+++ book-app/src/Components/Header.jsx
@@ -1,16 +1,40 @@
 import React from 'react'
+import styled from 'styled-components'
 
+const Nav = styled.div`
+	background-color: red;
+	display: flex;
+	align-items: center;
+    justify-content: flex-end;
+    padding: 20px;
+`
 
+const Heading = styled.h1`
+    display: flex;
+    margin: 0 auto 0 5px;
+`
+
+const NavList = styled.div`
+    display: flex;
+    justify-content: space-around;
+`
+
+const List = styled.li`
+	list-style-type: none;
+	font-weight: bold;
+	font-size: 20px;
+	margin: 0 10px;
+`
 
 export default function Header() {
 	return (
-		<nav className='nav-container'>
-			<h1>Book App</h1>
-			<div className='nav-links'>
-				<li>Home</li>
-				<li>Books</li>
-				<li>About</li>
-			</div>
-		</nav>
+		<Nav>
+			<Heading>Book App</Heading>
+			<NavList>
+				<List>Home</List>
+				<List>Books</List>
+				<List>About</List>
+			</NavList>
+		</Nav>
 	)
 }
