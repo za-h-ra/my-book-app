f4de026fed1f728ecb8ce63703793eaa62e2ea44
zahrakhadijha <zahra.khan1@icloud.com>
2020-12-14 23:22:10 -0500

    install react app and create components

---
+++ book-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from './Components/Header'
+import Book from './Components/Book'
+import Footer from './Components/Footer'
+
+export default function App() {
+    return (
+        <div>
+            <h1>Hello World!</h1>
+        </div>
+    )
+}
