add5e71c3b2404ae53b9a382087c866b2cfccf5b
zahrakhadijha <zahra.khan1@icloud.com>
2020-12-16 16:43:17 -0500

    adjust body height to min-height of 100vh

--- book-app/src/App.js
+++ book-app/src/App.js
@@ -12,7 +12,10 @@ export default function App() {
 					img='https://target.scene7.com/is/image/Target/GUEST_22c193bf-5ae5-4e21-8454-a874afdcb9e3?wid=488&hei=488&fmt=pjpeg'
 					description="No matter your goals, Atomic Habits offers a proven framework for improving--every day. James Clear, one of the world's leading experts on habit formation, reveals practical strategies that will teach you exactly how to form good habits, break bad ones, and master the tiny behaviors that lead to remarkable results."
 				/>
-				<Book title='Atomic Habits' />
+				<Book
+					title='Trick Mirror'
+					img='https://booksovermind.com/wp-content/uploads/2019/08/tm.jpeg'
+				/>
 				<Book title='Atomic Habits' />
 				<Footer />
 			</div>
