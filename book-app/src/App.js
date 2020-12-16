62c080db6adbdd210ad3fd347258c13332e3cd54
zahrakhadijha <zahra.khan1@icloud.com>
2020-12-16 17:13:27 -0500

    style Book Container

--- book-app/src/App.js
+++ book-app/src/App.js
@@ -4,20 +4,25 @@ import Book from './Components/Book'
 import Footer from './Components/Footer'
 
 export default function App() {
-    return (
-			<div>
-				<Header />
-				<Book
-					title='Atomic Habits'
-					img='https://target.scene7.com/is/image/Target/GUEST_22c193bf-5ae5-4e21-8454-a874afdcb9e3?wid=488&hei=488&fmt=pjpeg'
-					description="No matter your goals, Atomic Habits offers a proven framework for improving--every day. James Clear, one of the world's leading experts on habit formation, reveals practical strategies that will teach you exactly how to form good habits, break bad ones, and master the tiny behaviors that lead to remarkable results."
-				/>
-				<Book
-					title='Trick Mirror'
-					img='https://booksovermind.com/wp-content/uploads/2019/08/tm.jpeg'
-				/>
-				<Book title='Atomic Habits' />
-				<Footer />
-			</div>
-		)
+	return (
+		<div>
+			<Header />
+			<Book
+				title='Atomic Habits'
+				img='https://images-na.ssl-images-amazon.com/images/I/91pR9wKJ3zL.jpg'
+				description="No matter your goals, Atomic Habits offers a proven framework for improving--every day. James Clear, one of the world's leading experts on habit formation, reveals practical strategies that will teach you exactly how to form good habits, break bad ones, and master the tiny behaviors that lead to remarkable results."
+			/>
+			<Book
+				title='Trick Mirror'
+				img='https://images-na.ssl-images-amazon.com/images/I/61SvxK1biuL.jpg'
+				description='Trick Mirror is an enlightening, unforgettable trip through the river of self-delusion that surges just beneath the surface of our lives. This is a book about the incentives that shape us, and about how hard it is to see ourselves clearly through a culture that revolves around the self. In each essay, Tolentino writes about a cultural prism: the rise of the nightmare social internet; the advent of scamming as the definitive millennial ethos; the literary heroine’s journey from brave to blank to bitter; the punitive dream of optimization, which insists that everything, including our bodies, should become more efficient and beautiful until we die. Gleaming with Tolentino’s sense of humor and capacity to elucidate the impossibly complex in an instant, and marked by her desire to treat the reader with profound honesty, Trick Mirror is an instant classic of the worst decade yet.'
+			/>
+			<Book
+				title='Shoe Dog'
+				img='https://prodimage.images-bn.com/pimages/9781501135927_p0_v4_s1200x630.jpg'
+				description='Fresh out of business school, Phil Knight borrowed fifty dollars from his father and launched a company with one simple mission: import high-quality, low-cost running shoes from Japan. Selling the shoes from the trunk of his car in 1963, Knight grossed eight thousand dollars that first year. Today, Nike’s annual sales top $30 billion. In this age of start-ups, Knight’s Nike is the gold standard, and its swoosh is one of the few icons instantly recognized in every corner of the world.'
+			/>
+			<Footer />
+		</div>
+	)
 }
--- book-app/src/Components/Book.jsx
+++ book-app/src/Components/Book.jsx
@@ -1,11 +1,50 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const BookContainer = styled.div`
+	border: 5px solid #ff9672;
+	margin: 30px;
+	padding: 20px;
+    background-color: #ffc75e;
+    min-height: 50vh;
+    width: 60%;
+    display: flex;
+    flex-flow: column;
+    justify-content: space-around;
+    align-items: center;
+    
+`
+
+const BookTitle = styled.h1`
+    text-align: center;
+    font-size: 50px;
+`
+
+const BookContent = styled.div`
+    margin: 20px;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+`
+
+const BookImage = styled.img`
+    height: 300px;
+    width: 200px;
+`
+
+const BookText = styled.p`
+    margin: 40px;
+    font-weight: bold;
+`
 
 export default function Book(props) {
-    return (
-        <div>
-            <h2>{props.title}</h2>
-            <img src={props.img} />
-            <p>{props.description}</p>
-        </div>
-    )
+	return (
+		<BookContainer>
+			<BookTitle>{props.title}</BookTitle>
+			<BookContent>
+				<BookImage src={props.img} />
+				<BookText>{props.description}</BookText>
+			</BookContent>
+		</BookContainer>
+	)
 }
