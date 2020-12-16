a0fa789b37935865b12a62878bf8cf72dc432838
zahrakhadijha <zahra.khan1@icloud.com>
2020-12-16 17:30:47 -0500

    create and styled Footer Component

--- book-app/src/Components/Footer.jsx
+++ book-app/src/Components/Footer.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const FooterContainer = styled.footer`
+	background-color: #ffc75e;
+	width: 100%;
+	height: 50px;
+	positon: fixed;
+	bottom: 0;
+	box-shadow: 0px 0px 10px rgb(158, 158, 158);
+	z-index: -2;
+`
 
 export default function Footer() {
     return (
-        <div>
-            <h1>Footer</h1>
-        </div>
+        <FooterContainer>
+        </FooterContainer>
     )
 }
